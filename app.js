979fa3e03bee86ff44c9062e73616677535fac84
Dami Lee <dam1007@naver.com>
2022-09-24 02:03:04 +0900

    마우스 이벤트로 선 그리기

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 800;
-canvas.heigth = 800;
+canvas.height = 800;
+
+const colors = [
+    '#55efc4',
+    '#81ecec',
+    '#74b9ff',
+    '#a29bfe',
+    '#00cec9',
+    '#6c5ce7',
+    '#fd79a8',
+    '#e84393',
+    '#d63031',
+    '#ff7675',
+    '#fab1a0',
+    '#ffeaa7'
+];
+
+function onClick(e) {
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    
+    ctx.strokeStyle = color;
+    ctx.moveTo(0,0);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    ctx.beginPath();
+}
+
+canvas.addEventListener('mousemove', onClick);
